79ae3b7fc0b76b14ce51e0e1b4e1db8592c9ddc7
Raihan Iyai <raihaniyai@gmail.com>
2019-06-09 21:25:17 +0700

    add printing post and total of like/comment

--- instagram.py
+++ instagram.py
@@ -17,9 +17,22 @@ data = r.json()
 username = data['graphql']['user']['username']
 full_name = data['graphql']['user']['full_name']
 biography = data['graphql']['user']['biography']
+profile_pic_url_hd = data['graphql']['user']['profile_pic_url_hd']
+external_url = data['graphql']['user']['external_url']
 
-
-# printing the output
+# printing biodata of user
+print("Photo Profile\t: " + profile_pic_url_hd)
 print("Username\t: " + username)
 print("Full Name\t: " + full_name)
 print("Biography\t: " + biography)
+print("Website\t\t: " + external_url)
+
+i = 0
+
+# printing posts and total of like each post (9 last posts)
+for post in data['graphql']['user']['edge_owner_to_timeline_media']['edges']:
+    print("Post\t\t: " + post['node']['display_url'])
+    print("Likes\t\t: " + str(post['node']['edge_liked_by']['count']))
+    # print("Caption\t: " + post['node']['edge_media_to_caption']['edges'][0]['node']['text'])
+    print("Comments\t: " + str(post['node']['edge_media_to_comment']['count']) + "\n\n")
+    print("------------------------------")
