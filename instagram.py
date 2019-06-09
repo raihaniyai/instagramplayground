20687059661493c091c0aaed3eef7b2e64fa9fde
Raihan Iyai <raihaniyai@gmail.com>
2019-06-09 22:55:57 +0700

    adding most liked and commented post

--- instagram.py
+++ instagram.py
@@ -13,26 +13,72 @@ r = requests.get(url = URL)
 
 # extracting data in json format
 data = r.json()
+
 # getting variable needed
 username = data['graphql']['user']['username']
 full_name = data['graphql']['user']['full_name']
 biography = data['graphql']['user']['biography']
 profile_pic_url_hd = data['graphql']['user']['profile_pic_url_hd']
 external_url = data['graphql']['user']['external_url']
+followings = str(data['graphql']['user']['edge_follow']['count'])
+followers = str(data['graphql']['user']['edge_followed_by']['count'])
+user_id = data['graphql']['user']['id']
 
 # printing biodata of user
+print("\n===== BIODATA =====\n")
 print("Photo Profile\t: " + profile_pic_url_hd)
 print("Username\t: " + username)
 print("Full Name\t: " + full_name)
-print("Biography\t: " + biography)
-print("Website\t\t: " + external_url)
+# print("Biography\t: " + biography)
+# print("Website\t\t: " + external_url)
+print("Followers\t: " + followers)
+print("Followings\t: " + followings)
+print("\n===== POSTS =====\n")
 
-i = 0
+# getting posts of instagram user according to username
+POSTS = "https://www.instagram.com/graphql/query/?query_hash=f2405b236d85e8296cf30347c9f08c2a&variables={%22id%22:%22" + user_id + "%22,%22first%22:50}"
+req = requests.get(url = POSTS)
+data = req.json()
+
+i = 0 # only to make sure the num of posts is right
+most_like = 0
+most_comment = 0
 
 # printing posts and total of like each post (9 last posts)
-for post in data['graphql']['user']['edge_owner_to_timeline_media']['edges']:
-    print("Post\t\t: " + post['node']['display_url'])
-    print("Likes\t\t: " + str(post['node']['edge_liked_by']['count']))
-    # print("Caption\t: " + post['node']['edge_media_to_caption']['edges'][0]['node']['text'])
-    print("Comments\t: " + str(post['node']['edge_media_to_comment']['count']) + "\n\n")
-    print("------------------------------")
+while True:
+    # getting posts
+    for post in data['data']['user']['edge_owner_to_timeline_media']['edges']:
+        print("Post\t\t: " + post['node']['display_url'])
+        print("Likes\t\t: " + str(post['node']['edge_media_preview_like']['count']))
+        # print("Caption\t: " + post['node']['edge_media_to_caption']['edges'][0]['node']['text'])
+        print("Comments\t: " + str(post['node']['edge_media_to_comment']['count']) + "\n")
+        print("------------------------------\n")
+        i += 1
+
+        if most_like < post['node']['edge_media_preview_like']['count']:
+            most_like = post['node']['edge_media_preview_like']['count']
+            liked = "https://instagram.com/p/" + post['node']['shortcode']
+            pass
+        if most_comment < post['node']['edge_media_to_comment']['count']:
+            most_comment = post['node']['edge_media_to_comment']['count']
+            commented = "https://instagram.com/p/" + post['node']['shortcode']
+            pass
+        # end for
+
+    if data['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page'] == False:
+        break
+    else:
+        end_cursor = data['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
+        POSTS = "https://www.instagram.com/graphql/query/?query_hash=f2405b236d85e8296cf30347c9f08c2a&variables={%22id%22:%22" + user_id + "%22,%22first%22:50,%22after%22:%22" + end_cursor + "%22}"
+        req = requests.get(url = POSTS)
+        data = req.json()
+    # end while
+
+if i > 0:
+    print("Total Photos\t: " + str(i))
+    print("The Most Liked Post\t:" + liked)
+    print("Total of Likes\t\t: " + str(most_like) + "\n")
+    print("The Most Commented Post\t:" + commented)
+    print("Total of Comments\t\t: " + str(most_comment) + "\n")
+else:
+    print(username + " hasn't upload a photo/video yet or the account is locked")
